77a609afff76b0f0e0d0ef9f536435601f533839
HariharanS <s.hariharan16@gmail.com>
2016-11-19 11:59:05 +1100

    fix bug with tax calculation

--- EmployeePayDetailsDomain/Models/SalarySlip.cs
+++ EmployeePayDetailsDomain/Models/SalarySlip.cs
@@ -7,7 +7,7 @@ namespace EmployeePayDetailsDomain.Models
 {
     public class SalarySlip : ISalarySlip
     {
-        private readonly Salary _salary;
+        private readonly ISalary _salary;
         private readonly ITaxStructure _taxStructure;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
@@ -16,13 +16,13 @@ namespace EmployeePayDetailsDomain.Models
         public decimal IncomeTax => CalculateIncomeTax();
         public decimal Super => GrossIncome * _salary.SuperRate;
 
-        private SalarySlip(Salary salary, ITaxStructure taxStructure)
+        private SalarySlip(ISalary salary, ITaxStructure taxStructure)
         {
             _salary = salary;
             _taxStructure = taxStructure;
         }
 
-        public static SalarySlip CreateSalarySlip(Salary salary,ITaxStructure taxStructure) => new SalarySlip(salary, taxStructure);
+        public static SalarySlip CreateSalarySlip(ISalary salary,ITaxStructure taxStructure) => new SalarySlip(salary, taxStructure);
 
         private decimal CalculateIncomeTax()
         {
@@ -33,19 +33,19 @@ namespace EmployeePayDetailsDomain.Models
                             .TaxSlab
                             .FirstOrDefault(x => x.MinIncome >= _salary.AnnualSalary && 
                                                 x.MaxIncome <= _salary.AnnualSalary);
-            if(taxSlab == null) 
+            // check if tax slab is null and throw an exception
+            if (taxSlab == null) throw new NullReferenceException("Tax Slab is null");
             // no tax
             if (taxSlab.Tax == null)
                 return incomeTax;
-
+            //get tax structure
             var tax = taxSlab.Tax;
-
-            var annualTax = (_salary.AnnualSalary - taxSlab.MinIncome) *
-                            (tax.AdditionalTaxPercentageOverMinIncome.Value);
-
-            var monthlyTax = tax.FlatTax == null ? 
-                                Math.Round(annualTax/12, 0, MidpointRounding.AwayFromZero) : 
-                                tax.FlatTax + Math.Round(annualTax / 12, 0, MidpointRounding.AwayFromZero);
+            
+            var annualTaxPartial = ((_salary.AnnualSalary - taxSlab.MinIncome) * (tax.AdditionalTaxPercentageOverMinIncome.Value));
+            // calculate annual tax
+            var annualTax = tax.FlatTax == null ? annualTaxPartial  : tax.FlatTax + annualTaxPartial;
+            // calculate monthly tax
+            var monthlyTax =  Math.Round(annualTax.Value/12, 0, MidpointRounding.AwayFromZero);
 
             return (decimal) monthlyTax;
         }
